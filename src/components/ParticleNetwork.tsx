a0d74d01fed90e7049672e74b26f233e92824ce0
VHarshB <hvaishya@asu.edu>
2026-01-28 22:34:30 -0700

    Add descriptive comment for connection nodes section

--- src/components/ParticleNetwork.tsx
+++ src/components/ParticleNetwork.tsx
@@ -118,7 +118,8 @@ const CircuitAnimation: React.FC = () => {
       animatedLines.push({ line: animLine, path });
     });
 
-    // Add connection nodes
+    // Add connection nodes at circuit path junction points
+    // These nodes represent connection points along the circuit paths
     const nodeGeometry = new THREE.CircleGeometry(0.3, 16);
     paths.forEach(path => {
       path.points.forEach((point, idx) => {
